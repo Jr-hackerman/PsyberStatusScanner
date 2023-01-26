aeff3e65febb3b588ecace583e9f2cd21d0a8c1c
PsyberBook <103625379+researchersiri@users.noreply.github.com>
2023-01-26 22:27:51 +0530

    new version v1.1.0 updated
    new version v1.1.0 updated  with Threads limits and timeout 
    -t TIMEOUT, --timeout TIMEOUT
                            Timeout for the HTTP requests
      -rl NUM_THREADS, --num_threads NUM_THREADS
                            Number of threads for the scanning

--- psyberstatusscanner.py
+++ psyberstatusscanner.py
@@ -1,9 +1,14 @@
+# psyberstatus scanner v1.1.0
+# Created by jr_hackerman
+# also checkout psyberbook.com
+
 import argparse
 import requests
 from prettytable import PrettyTable
 from tqdm import tqdm
-print("""
+import concurrent.futures
 
+print("""
 
  888888ba                    dP                         .d88888b    dP              dP                        
  88    `8b                   88                         88.    "'   88              88                        
@@ -20,11 +25,8 @@ oooooooooooo.d88888bo~~~~.88~ooooooooooooooooooooooooooooooooooooooooooooooooooo
 oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo - Efficiently scan and analyze large lists of domains with PsyberStatus Scanner
          
 PsyberStatus Scanner is intended for legitimate and ethical use only. The creators of this tool are not responsible for any unauthorized or illegal use of this tool. By using PsyberStatus Scanner, you agree to use it only for lawful purposes and in compliance with all applicable laws.               
-	""")                                                                                        
-                                                                                                                            
-
-
-def check_status_codes_from_file(file_path, timeout):
+	""")                                                                                                                                                                                                                    
+def check_status_codes_from_file(file_path, timeout, num_threads):
     try:
         with open(file_path, 'r') as f:
             domains = f.read().splitlines()
@@ -35,33 +37,34 @@ def check_status_codes_from_file(file_path, timeout):
     x = PrettyTable()
     x.field_names = ["Domain", "HTTP", "HTTPS", "Redirection Location"]
     try:
-        for domain in tqdm(domains, desc="Scan Progress", unit="domain"):
-            protocol_status_code = {"http": None, "https": None}
-            redirection_location = None
-            for protocol in ["http", "https"]:
-                url = f"{protocol}://{domain}"
-                try:
-                    response = requests.get(url, timeout=timeout, allow_redirects=False)
-                    status_code = response.status_code
-                    protocol_status_code[protocol] = status_code
-                    if status_code in [301, 302]:
-                        redirection_location = response.headers.get('location')
-                except requests.exceptions.RequestException as e:
-                    protocol_status_code[protocol] = "unreachable"
-            x.add_row([domain, protocol_status_code["http"], protocol_status_code["https"], redirection_location])
+        with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+            results = [executor.submit(check_status_code, domain, timeout) for domain in domains]
+            for f in tqdm(concurrent.futures.as_completed(results), total=len(results)):
+                domain, protocol_status_code, redirection_location = f.result()
+                x.add_row([domain, protocol_status_code["http"], protocol_status_code["https"], redirection_location])
     except KeyboardInterrupt:
         print("\nScan Cancelled by user")
     print(x)
-    
-    
 
-if __name__ == "__main__":
-  
-    parser = argparse.ArgumentParser(description="Check status codes of domains from a file")
-    parser.add_argument("-dl", "--domains-list", required=True, help="File containing the list of domains")
-    parser.add_argument("-t", "--timeout", type=int, default=5, help="Timeout for the HTTP request")
+def check_status_code(domain, timeout):
+    protocol_status_code = {"http": None, "https": None}
+    redirection_location = None
+    for protocol in ["http", "https"]:
+        url = f"{protocol}://{domain}"
+        try:
+            response = requests.get(url, timeout=timeout, allow_redirects=False)
+            status_code = response.status_code
+            protocol_status_code[protocol] = status_code
+            if status_code in [301, 302]:
+                redirection_location = response.headers.get('location')
+        except requests.exceptions.RequestException as e:
+            protocol_status_code[protocol] = "unreachable"
+    return domain, protocol_status_code, redirection_location
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Check status codes for a list of domains")
+    parser.add_argument("-dl", "--domain_list", type=str, help="File path of the list of domains", required=True)
+    parser.add_argument("-t", "--timeout", type=int, help="Timeout for the HTTP requests", default=5)
+    parser.add_argument("-rl", "--num_threads", type=int, help="Number of threads for the scanning", default=10)
     args = parser.parse_args()
-
-    file_path = args.domains_list
-    timeout = args.timeout
-    results = check_status_codes_from_file(file_path, timeout)
+    check_status_codes_from_file(args.domain_list, args.timeout, args.num_threads)
